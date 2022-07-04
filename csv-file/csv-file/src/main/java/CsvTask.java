95a335f3db07aa42cd6415b4b6d6b09520159a61
kiranteja7 <88424099+kiranteja7@users.noreply.github.com>
2022-07-04 16:09:36 +0530

    changes made on csv file

--- csv-file/csv-file/src/main/java/CsvTask.java
+++ csv-file/csv-file/src/main/java/CsvTask.java
@@ -1,12 +1,14 @@
 import org.apache.commons.csv.CSVParser;
-import utils.csv;
+import utility.csv;
 
-import java.sql.Array;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CsvTask {
     public static void main(String[] args) {
         csv obj = new csv();
+
         String file1 = "C:\\Users\\tumkiran\\buddygraduate\\assignment\\assignment\\csv-file\\csv-file\\src\\main\\resources\\datafiles\\readInput.csv";
         String file2 = "C:\\Users\\tumkiran\\buddygraduate\\assignment\\assignment\\csv-file\\csv-file\\src\\main\\resources\\datafiles\\readInput2.csv";
         CSVParser csvParser1 = obj.loadFile(file1);
@@ -20,7 +22,7 @@ public class CsvTask {
         for (int i=0; i<header2.size(); i++){
             final_header.put(header2.get(i), 2);
         }
-     /*  for (String key: final_header.keySet()
+     /* for (String key: final_header.keySet()
             ) {
           System.out.println(key);
         }*/
@@ -63,9 +65,11 @@ public class CsvTask {
              ) {
                // file3Content += rowFile1.get(final_data.get(key)[0]+rowFile2.get(final_data.get(key)[1]);
                 file3Content += getData(rowFile1,final_data.get(key)[0])+getData(rowFile2,final_data.get(key)[1])+",";
+
         }
            file3Content+="\n";
         }
+        System.out.println("csv parser");
 
 //        for (String key: final_data.keySet()
 //             ) {
@@ -75,6 +79,7 @@ public class CsvTask {
 //        key Population 2 -1
 //        key Rankings -1 2
 //        key Capital 1 1
+
 
        obj.fileWriter("file3.csv", file3Content);
     }
